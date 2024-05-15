1782693cb00db83bb5c12788443ecbb8a095fb43
M1r12on <marloncadell@gmx.de>
2024-05-15 02:30:50 +0200

    Complete home page layout without filter popup

--- src/views/SucheView.vue
+++ src/views/SucheView.vue
@@ -1,108 +1,217 @@
 <template>
-  <div class="leftUpIcons">
-    <AccountIcon />
-    <MailIcon />
-  </div>
-  <div id="RightUpButton">
-    <button id="ButtonErstellen">Event erstellen</button>
-  </div>
-  <div class="image">
-    <LoginComponent />
-  </div>
+    <div id="header">
+        <div id="profile-mail">
+            <div class = "icon-div">
+                <img alt="Filer" class="icon" src="../assets/profile-icon.png">
+            </div>
+            <div class = "icon-div">
+                <img alt="Filer" class="icon" src="../assets/mail-icon.png">
+            </div>
+        </div>
+        <div id= "new-event">
+            <button id="new-event-button">Event erstellen</button>
+        </div>
+    </div>
 
-  <div id="formLayout">
-    <select id="filterOptions">
-      <option value="1">Location</option>
-      <option value="2">DJ/Band</option>
-      <option value="3">Caterer</option>
-      <option value="4">Event</option>
-      <option value="5">Person</option>
-      <option value="6">Meine Events</option>
-      <option value="7">Meine Tickets</option>
-      <option value="8">Meine Freunde</option>
-      <option value="9">Meine Locations</option>
-    </select>
-
-    <input class="searchBar" type="search" name="seach" placeholder="Hier suchen... "/>
-
-  </div>
+    <div id="login-component">
+        <LoginComponent />
+    </div>
+    
+    <div id="selct-filter-searchbar">
+        <div id="select-filter">
+            <select class="options">
+                <option value="1">Location</option>
+                <option value="2">DJ/Band</option>
+                <option value="3">Caterer</option>
+                <option value="4">Event</option>
+                <option value="5">Person</option>
+                <option value="6">Meine Events</option>
+                <option value="7">Meine Tickets</option>
+                <option value="8">Meine Freunde</option>
+                <option value="9">Meine Locations</option>
+            </select>
+        </div>
+    
+        <div id="searchbar-and-icons">
+            <div>
+                <img alt="Filer" id="filter" src="../assets/filter.jpg">
+            </div>
+            <div>
+                <input id="searchbar" type="search" name="seach" placeholder="Hier suchen... "/>
+            </div>
+            <div>
+                <img alt="Magnifying Glass" id="magnifying-glass" src="../assets/magnifying-glass.jpg">
+            </div>
+        </div>
+    </div>
+    
+    <div id="bookmark-arrow">
+        <div class="bookmark-arrow-div">
+            <img alt="Bookmark White" id="bookmark-white" src="../assets/bookmark-white.jpg">
+        </div>
+        <div id="select-sort">
+            <select class="options">
+                <option value="1">Location</option>
+                <option value="2">DJ/Band</option>
+                <option value="3">Caterer</option>
+                <option value="4">Event</option>
+                <option value="5">Person</option>
+                <option value="6">Meine Events</option>
+                <option value="7">Meine Tickets</option>
+                <option value="8">Meine Freunde</option>
+                <option value="9">Meine Locations</option>
+            </select>
+        </div>
+        <div class="bookmark-arrow-div">
+            <img alt="Normal Arrow" id="normal-arrow" src="../assets/normal-arrow.jpg">
+            </div>
+    </div>
 </template>
+
 
 <script>
 import LoginComponent from '@/components/LoginComponent';
-import AccountIcon from '@/components/AccountIconComponent';
-import MailIcon from '@/components/MailIconComponent';
 
 export default {
   components: {
-    LoginComponent,
-    AccountIcon,
-    MailIcon,
+    LoginComponent
   },
 };
 </script>
 
+
 <style scoped>
-
-#filterOptions{
-  box-shadow: 1px 1px 5px rgba(0,0,0,0.1); 
-  border: 1.5px solid #989b98; 
-  cursor: pointer; 
-  width:90px;
-  height:30px;
-  border-radius: 45px;
+#header{
+    display: grid;
+    grid-template-columns: auto auto;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 30px;
 }
 
-#formLayout {
-  position: relative;
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  gap: 10px;
-  color: grey;
-  left: 330px;
-  top: 50px;
-  width: 650px;
-  height: 40px;
-  border-radius: 20px;
+.icon-div{
+    padding: 15px;
+    padding-bottom: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+    border-radius: 10px;
+    cursor: pointer; 
 }
 
-
-
-.searchBar {
-  border-radius: 20px;
-  width: 100%;
-  height: 30px;
-  border: 1.0px solid #cccccc;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
-  padding-left: 30px; /* Adjust padding to accommodate icon */
-  position: relative; /* Make the searchBar position relative */
+.icon{
+    width: 35px;
+    height: 35px;
+    cursor: pointer; 
 }
 
-
-
-.leftUpIcons {
-  position: absolute;
-  display: flex;
-  width: 150px;
-  top: 40px;
-  left: 25px;
-  justify-content: space-around;
+#new-event-button{
+    border-radius: 5px;
+    width: 160px;
+    height: 42.5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+    border: 1px solid #000000; 
+    cursor: pointer; 
+    text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    font-weight: 600;
+    margin-right: 100px;
 }
 
-#RightUpButton {
-  position: absolute;
-  display: flex;
-  width: 140px;
-  top: 48px;
-  right: 70px;
+#profile-mail{
+    display: grid;
+    grid-template-columns: auto auto;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    margin-left: 50px;
 }
 
-#ButtonErstellen {
-  border-radius: 12px;
-  width: 130px;
-  height: 30px;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
-  border: 1.5px solid #989b98;
-  cursor: pointer;
+#login-component{
+    margin-top: 150px;
+}
+
+#selct-filter-searchbar{
+    margin-top: 100px;
+    display: grid;
+    grid-template-columns: auto auto;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-right: 100px;
+    margin-right: 0px;
+}
+
+#select-filter{
+    display: grid;
+    grid-template-columns: auto;
+    align-items: center;
+    justify-content: end;
+}
+
+#searchbar-and-icons{
+    display: grid;
+    grid-template-columns: auto auto auto;
+    align-items: center;
+    justify-content: left;
+    gap: 10px;
+
+    border-radius: 20px;
+    border: 1.0px solid #cccccc;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    height: 35px;
+    width: 600px;
+    padding-left: 10px;
+}
+
+.options{
+    border-radius: 20px;
+    border: 1.0px solid #cccccc;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    cursor: pointer; 
+    width: 130px;
+    height: 35px;
+    border-radius: 45px;
+    text-align: center;
+}
+
+#searchbar{
+    width: 510px;
+    height: 30px;
+    border: 0px;
+}
+
+#searchbar:focus{
+    outline: none;
+}
+
+#bookmark-arrow{
+    display: grid;
+    grid-template-columns: auto auto auto;
+    align-items: center;
+    justify-content: left;
+    gap: 10px;
+    margin-top: 100px;
+    margin-left: 30px;
+}
+
+.bookmark-arrow-div{
+    border-radius: 20px;
+    border: 1.0px solid #cccccc;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+    cursor: pointer; 
+    width: 40px;
+    height: 35px;
+    display: grid;
+    grid-template-columns: auto;
+    align-items: center;
+    justify-content: center;
+}
+
+#bookmark-white{
+    width: 15px;
+    height: 30px;
+}
+
+#normal-arrow{
+    width: 20px;
+    height: 30px;
 }
 </style>
