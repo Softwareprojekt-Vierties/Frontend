987e524f271a5a5517527cd00f6f67e847039b17
Lucas <lucas.freeride6@gmail.com>
2024-06-06 01:24:26 +0200

    Implemented event creation page logic, location submission to database not yet included.

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -5,23 +5,23 @@
         <img alt="Filer" class="icon" src="../assets/home.jpg">
       </div>
       <div id="picture-name">
-        <div id="file-div">
+        <div id="file-div" :style="fileDivStyle">
           <div id="file-upload">
             <label id="image-text" for="fileToUpload">
-              <img src="../assets/addpicture.jpg" alt="Bild hochladen" class="upload-icon" />
-              <span id="upload-text">Bild hochladen</span>
+              <img v-if="!imagePreview" src="../assets/addpicture.jpg" alt="Bild hochladen" class="upload-icon" />
+              <span id="upload-text" v-if="!imagePreview">Bild hochladen</span>
             </label>
-            <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*">
+            <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*" @change="onFileChange">
           </div>
         </div>
         <div id="name-description">
           <div class="name-description-input">
             <label class="description">Eventnamen hinzufügen:</label>
-            <input class="header-input" type="text" placeholder="z.B. UNI PARTY"><br>
+            <input class="header-input" v-model="eventName" type="text" placeholder="z.B. UNI PARTY"><br>
           </div>
           <div class="name-description-input">
             <label class="description">Kurze Beschreibung hinzufügen:</label>
-            <input class="header-input" type="text" placeholder="z.B. Minden">
+            <input class="header-input" v-model="shortDescription" type="text" placeholder="z.B. Minden">
           </div>
         </div>
       </div>
@@ -31,17 +31,19 @@
       <div id="left-side">
         <div class="long-description">
           <label class="description">Beschreibung hinzufügen:</label>
-          <textarea id="long-description-input" type="text" placeholder="Hier einfügen…"></textarea>
+          <textarea id="long-description-input" v-model="longDescription" type="text" placeholder="Hier einfügen…"></textarea>
         </div>
         <br>
         <div class="long-description">
           <label class="description">Dienstleister hinzufügen:</label>
           <div id="addcreator" ref="addCreator" class="scroll-container">
             <div class="dish-container">
-              <div v-for="(dish, index) in dishes" :key="index" class="dish-item">
-                <dish-form :dish="dish" @remove="removeDish(index)"></dish-form>
+              <div v-for="(serviceProvider, index) in serviceProvider" :key="index" class="dish-item">
+                <dish-form :serviceProvider="serviceProvider" @remove="removeProvider(index)"></dish-form>
               </div>
-              <div class="add-dish-button" @click="addDish"><img src="../assets/addlocation.jpg" alt="Bild hochladen" id="add-icon" /></div>
+              <div class="add-dish-button" @click="addProvider">
+                <img src="../assets/addlocation.jpg" alt="Bild hochladen" id="add-icon" />
+              </div>
             </div>
           </div>
         </div>
@@ -59,34 +61,38 @@
           </div>
           <div class="infos">
             <label class="info-subheadline">Datum:</label>
-            <input class="input" type="text" placeholder="z.B. 17.08.2024">
+            <input class="input" v-model="eventDate" type="text" placeholder="z.B. 17.08.2024">
           </div>
           <div class="infos">
             <label class="info-subheadline">Zeit:</label>
             <div class="time">
-              <input class="time-value-left" placeholder="z.B. 17Uhr"> - <input class="time-value-right" placeholder="z.B. 20Uhr">
+              <input class="time-value-left" v-model="eventStartTime" placeholder="z.B. 17Uhr"> - 
+              <input class="time-value-right" v-model="eventEndTime" placeholder="z.B. 20Uhr">
             </div>
           </div>
           <div class="infos">
             <label class="info-subheadline">Eventgröße:</label>
-            <input class="input" type="text" placeholder="z.B. 50 Personen">
+            <input class="input" v-model="eventSize" type="text" placeholder="z.B. 50 Personen">
           </div>
           <div class="infos">
             <label class="info-subheadline">Preis:</label>
-            <input class="input" type="text" placeholder="z.B. 50€">
+            <input class="input" v-model="price" type="text" placeholder="z.B. 50€">
           </div>
           <div class="infos">
             <label class="info-subheadline">Alter:</label>
-            <input class="input" type="text" placeholder="z.B. 18 Jahre">
+            <input class="input" v-model="ageLimit" type="text" placeholder="z.B. 18 Jahre">
           </div>
           <div id="open-air">
             <label class="info-subheadline">Open Air:</label>
-            <label class="switch"> <input type="checkbox"> <span class="slider round"> </span> </label>
+            <label class="switch">
+              <input type="checkbox" v-model="openAir">
+              <span class="slider round"></span>
+            </label>
           </div>
         </div>
         <div id="buttons">
-          <div id="break">
-            abbrechen
+          <div id="break" @click="resetForm">
+            abbrechen  
           </div>
           <div id="continue" @click="openModal">
             anlegen
@@ -95,43 +101,145 @@
       </div>
     </div>
 
-    <PopupModal :show="isModalVisible" @close="closeModal"/>
+    <!-- Integrated PopupModal functionality -->
+    <div v-if="isModalVisible" id="modal-overlay">
+      <div id="modal-content">
+        <div id="headline">
+          <label>Privatsphäre-Einstellung</label>
+        </div>
+        <div id="event-buttons">
+          <div id="event-button" @click="selectEventType('private')" :class="{ 'selected': selectedEventType === 'private' }">privates Event</div>
+          <div id="event-button" @click="selectEventType('public')" :class="{ 'selected': selectedEventType === 'public' }">öffentliches Event</div>
+        </div>
+        <div id="action-buttons">
+          <button id="cancel-button" @click="closeModal">abbrechen</button>
+          <button id="create-button" @click="createEvent">Jetzt erstellen</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
 <script>
+import axios from 'axios';
 import DishForm from '../components/DishForm.vue';
-import PopupModal from '../components/PopupModal.vue'; // Importiere die neue Komponente
 
 export default {
   components: {
     DishForm,
-    PopupModal
   },
   data() {
     return {
-      dishes: [
-        { name: '', ingredients: [] }
+      eventName: '',
+      shortDescription: '',
+      longDescription: '',
+      eventImage: null,
+      imagePreview: null, 
+      eventDate: '',
+      eventStartTime: '',
+      eventEndTime: '',
+      eventSize: '',
+      price: '',
+      ageLimit: '',
+      openAir: false,
+      serviceProvider: [
+        { name: '', details: [] }
       ],
-      isModalVisible: false
+      isModalVisible: false,
+      selectedEventType: null 
+
     };
   },
+
+  computed: {
+    fileDivStyle() {
+      return this.imagePreview ? { backgroundImage: `url(${this.imagePreview})`, backgroundSize: 'cover', backgroundPosition: 'center' } : {};
+    }
+  },
+
+
   methods: {
-    addDish() {
-      this.dishes.push({ name: '', ingredients: [] });
+
+    onFileChange(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.imagePreview = URL.createObjectURL(file); // Set the image preview
+        this.uploadedImage = file; // Store the actual file for upload
+      }
+    },
+    addProvider() {
+      this.serviceProvider.push({ name: '', details: [] });
       this.$nextTick(() => {
-        const container = this.$refs.addCreator; // Verwendet ref, um den Container zu referenzieren
-        container.scrollLeft = container.scrollWidth - container.clientWidth; // Scrollt zum rechten Ende des Containers
+        const container = this.$refs.addCreator;
+        container.scrollLeft = container.scrollWidth - container.clientWidth;
       });
     },
-    removeDish(index) {
-      this.dishes.splice(index, 1);
+    removeProvider(index) {
+      this.serviceProvider.splice(index, 1);
     },
     openModal() {
       this.isModalVisible = true;
     },
     closeModal() {
       this.isModalVisible = false;
+    },
+    selectEventType(type) { 
+      this.selectedEventType = type;
+    },
+    resetForm() {
+      this.eventName = '';
+      this.shortDescription = '';
+      this.longDescription = '';
+      this.eventImage = null;
+      this.imagePreview = null; 
+      this.eventDate = '';
+      this.eventStartTime = '';
+      this.eventEndTime = '';
+      this.eventSize = '';
+      this.price = '';
+      this.ageLimit = '';
+      this.openAir = false;
+      this.serviceProviders = [{ name: '', ingredients: [] }];
+      this.selectedEventType= null; 
+    },
+    async createEvent() {
+      const formData = new FormData();
+      formData.append('eventName', this.eventName);
+      formData.append('shortDescription', this.shortDescription);
+      formData.append('longDescription', this.longDescription);
+      formData.append('eventDate', this.eventDate);
+      formData.append('eventStartTime', this.eventStartTime);
+      formData.append('eventEndTime', this.eventEndTime);
+      formData.append('eventSize', this.eventSize);
+      formData.append('price', this.price);
+      formData.append('ageLimit', this.ageLimit);
+      formData.append('openAir', this.openAir);
+      formData.append('selectedEventType', this.selectedEventType); 
+      if (this.uploadedImage) {
+        formData.append('eventImage', this.uploadedImage);
+      }
+
+      const serviceProvidersData = this.serviceProvider.map(provider => ({
+        name: provider.name,
+        details: provider.details
+      }));
+      formData.append('serviceProviders', JSON.stringify(serviceProvidersData));
+
+      const token = localStorage.getItem('authToken');
+
+      try {
+        const response = await axios.post('/createEvent', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        console.log('Event created:', response.data);
+        this.closeModal(); 
+      } catch (error) {
+        console.error('Error with Event creation:', error);
+      }
     }
   }
 }
@@ -477,5 +585,94 @@ input:checked + .slider:before {
 
 footer {
   background-color: rgb(242, 242, 242);
+}
+
+/* Integrated PopupModal styles */
+#modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  grid-template-columns: auto;
+  justify-content: center;
+  align-items: center;
+}
+
+#modal-content {
+  background: white;
+  padding: 30px;
+  padding-top: 20px;
+  padding-bottom: 12.5px;
+  border-radius: 20px;
+  text-align: center;
+  width: 300px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+}
+
+#headline {
+  margin-bottom: 50px;
+  font-weight: bold;
+}
+
+#event-buttons {
+  display: grid;
+  grid-template-columns: auto;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+#event-button {
+  border-radius: 50px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  cursor: pointer;
+  padding: 11px;
+  padding-left: 45px;
+  padding-right: 45px;
+  margin-bottom: 20px;
+  font-size: 12px;
+}
+
+#action-buttons {
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+#cancel-button {
+  background-color: rgb(254, 68, 77);
+  width: 115px;
+  height: 35px;
+  border-radius: 7px;
+  border: 1px solid #000000;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+#create-button {
+  background-color: rgb(146, 208, 80);
+  width: 115px;
+  height: 35px;
+  border-radius: 7px;
+  border: 1px solid #000000;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+#modal-content #event-buttons #event-button.selected {
+  background-color: blue; /* Tonalidade azul */
+  color: white; /* Ajuste a cor do texto se necessário */
 }
 </style>
