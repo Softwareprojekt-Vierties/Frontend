bd3c9cc8f244d699e98fd73f77c9e5c206140c01
M1r12on <marloncadell@gmx.de>
2024-06-11 19:31:21 +0200

    test

---
+++ src/views/EventView.vue
@@ -0,0 +1,373 @@
+<template>
+    <div id="app">
+      <div id="header">
+        <div id="icon-div">
+          <img alt="Filer" class="icon" src="../assets/home.jpg">
+        </div>
+        <div id="picture-name">
+          <div id="file-div">
+          </div>
+          <div id="name-description">
+            <div class="name-description-input">
+              <label id="name">Uni Party</label>
+            </div>
+            <div class="name-description-input">
+              <label id="description-short">Minden</label>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div id="main">
+        <div id="left-side">
+          <div class="long-description">
+            <label class="description">Beschreibung hinzufügen:</label>
+            <div id="long-description-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer</div>
+          </div>
+          <br>
+        </div>
+        <div id="right-side">
+          <div id="right-side-info">
+            <label id="info-headline">Infos hinzufügen:</label>
+            <div class="infos">
+              <label class="info-subheadline">Stadt:</label>
+              <input class="input" type="text" placeholder="z.B. 32427 Minden">
+            </div>
+            <div class="infos">
+              <label class="info-subheadline">Straße:</label>
+              <input class="input" type="text" placeholder="z.B. Artilleriestraße 9">
+            </div>
+            <div class="infos">
+              <label class="info-subheadline">Kapazität:</label>
+              <input class="input" type="text" placeholder="z.B. 50 Personen">
+            </div>
+            <div class="infos">
+              <label class="info-subheadline">Preis:</label>
+              <input class="input" type="text" placeholder="z.B. 50€">
+            </div>
+            <div class="infos">
+              <label class="info-subheadline">Größe:</label>
+              <input class="input" type="text" placeholder="z.B. 50 ha">
+            </div>
+            <div id="open-air">
+              <label class="info-subheadline">Open Air:</label>
+              <label class="switch"> <input type="checkbox"> <span class="slider round"> </span> </label>
+            </div>
+          </div>
+          <div id="buttons">
+            <div id="break">
+              abbrechen
+            </div>
+            <div id="continue">
+              anlegen
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    components: {
+    },
+    data() {
+      return {
+        dishes: [
+          { name: '', ingredients: [] }
+        ],
+        isModalVisible: false
+      };
+    },
+    methods: {
+      addDish() {
+        this.dishes.push({ name: '', ingredients: [] });
+        this.$nextTick(() => {
+          const container = this.$refs.addCreator; // Verwendet ref, um den Container zu referenzieren
+          container.scrollLeft = container.scrollWidth - container.clientWidth; // Scrollt zum rechten Ende des Containers
+        });
+      },
+      removeDish(index) {
+        this.dishes.splice(index, 1);
+      },
+      openModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  :root html, body {
+    width: 100%;
+    height: 100%;
+    background-color: rgb(242, 242, 242);
+  }
+  
+  #header {
+    background-color: rgb(213, 213, 213);
+    padding-bottom: 40px;
+    padding-top: 10px;
+  }
+  
+  #picture-name {
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: center;
+    align-items: end;
+    gap: 20px;
+  }
+  
+  #icon-div {
+    width: 40px;
+    padding: 15px;
+    padding-bottom: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+    border-radius: 10px;
+    cursor: pointer;
+    background-color: white;
+    margin-left: 10px;
+  }
+  
+  .icon {
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+  }
+  
+  #name-description {
+    padding: 10px;
+  }
+
+  #name {
+    text-align: left;
+    font-size: 35px;
+    color: white;
+    margin-bottom: 10px;
+  }
+
+  #description-short {
+    text-align: left;
+    font-size: 18px;
+    color: white;
+    margin-bottom: -10px;
+  }
+  
+  .name-description-input {
+    display: grid;
+    grid-template-columns: 300px;
+    justify-content: left;
+  }
+  
+  .description {
+    text-align: left;
+    margin-bottom: 3px;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  
+  #file-div {
+    width: 250px;
+    height: 180px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+    border-radius: 10px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: -225px;
+  }
+  
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 40px; /* Angepasst an die neue Höhe */
+    height: 20px;
+    margin-left: 20px;
+  }
+  
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+    margin-left: 70px;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 20px; /* Angepasst an die neue Höhe */
+  }
+  
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 16px; /* Angepasst an die neue Höhe */
+    width: 16px; /* Angepasst an die neue Höhe */
+    left: 2px; /* Angepasst an die neue Höhe */
+    bottom: 2px; /* Angepasst an die neue Höhe */
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+  }
+  
+  input:checked + .slider {
+    background-color: rgb(0, 217, 217);
+  }
+  
+  input:focus + .slider {
+    box-shadow: 0 0 1px rgb(0, 217, 217);
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(20px); /* Angepasst an die neue Höhe */
+    -ms-transform: translateX(20px); /* Angepasst an die neue Höhe */
+    transform: translateX(20px); /* Angepasst an die neue Höhe */
+  }
+  
+  #main {
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: center;
+    background-color: rgb(242, 242, 242);
+    padding-top: 30px;;
+    gap: 20px;
+  }
+  
+  .long-description {
+    border-radius: 10px;
+    background-color: white;
+    padding: 10px;
+    display: grid;
+    justify-content: left;
+    font-weight: bold;
+  }
+  
+  #right-side-info {
+    border-radius: 10px;
+    background-color: white;
+    padding: 10px;
+  }
+  
+  .infos {
+    display: grid;
+    grid-template-columns: 180px;
+    margin-top: 20px;
+  }
+  
+  .time-value-left {
+    width: 71.9px;
+    text-align: center;
+    border: 1px solid #000000; /* Rahmenstil */
+    border-radius: 5px; /* Abgerundete Ecken */
+    height: 25px;
+    margin-right: 5px;
+  }
+  
+  .time-value-right {
+    width: 71.9px;
+    text-align: center;
+    border: 1px solid #000000; /* Rahmenstil */
+    border-radius: 5px; /* Abgerundete Ecken */
+    height: 25px;
+    margin-left: 5px;
+  }
+  
+  .info-subheadline {
+    text-align: left;
+    font-size: 12px;
+  }
+  
+  .input {
+    text-align: center;
+    border: 1px solid #000000; /* Rahmenstil */
+    border-radius: 5px; /* Abgerundete Ecken */
+    height: 25px;
+  }
+  
+  #long-description-text {
+    width: 558px;
+    font-family: Arial, sans-serif;
+    font-size: 12px;
+    border-radius: 8px;
+    resize: none;
+    text-align: left;
+    text-decoration: none;
+    font-weight: lighter;
+  }
+  
+  #add-location-icon {
+    width: 25px;
+    height: 25px;
+    margin-top: 4px;
+  }
+  
+  #add-location {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: white;
+  }
+  
+  #info-headline {
+    display: block; /* Als Block-Element anzeigen */
+    text-align: left; /* Text links ausrichten */
+    font-family: Arial, sans-serif; /* Schriftart festlegen */
+    font-size: 14px; /* Schriftgröße festlegen */
+    font-weight: bold;
+  }
+  
+  #open-air {
+    display: block;
+    text-align: left;
+    margin-top: 20px;
+  }
+  
+  #buttons {
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin-top: 15px;
+  }
+  
+  #break {
+    background-color: rgb(254, 68, 77);
+    width: 88px;
+    height: 25px;
+    border-radius: 5px;
+    border: 1px solid #000000;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  
+  #continue {
+    background-color: rgb(146, 208, 80);
+    width: 88px;
+    height: 25px;
+    border-radius: 5px;
+    border: 1px solid #000000;
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  </style>
+  
