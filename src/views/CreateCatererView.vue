a4436ae4a65853dd05c3259e7816844609111f80
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2024-06-26 22:25:29 +0200

    Unrefactoring

--- src/views/CreateCatererView.vue
+++ src/views/CreateCatererView.vue
@@ -216,6 +216,441 @@ export default {
 
 <style scoped>
 
-@import "../css/createPages.css"
+#header {
+  background-color: var(--create-page-header-background);
+  padding-bottom: 40px;
+  padding-top: 10px;
+}
 
+#picture-name {
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  align-items: end;
+  gap: 20px;
+}
+
+#icon-div {
+  width: 40px;
+  padding: 15px;
+  padding-bottom: 12px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: var(--textfield-background);
+  margin-left: 10px;
+}
+
+.icon {
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+}
+
+#name-description {
+  border-radius: 10px;
+  background-color: var(--textfield-background);
+  padding: 10px;
+}
+
+.name-description-input {
+  display: grid;
+  grid-template-columns: 300px;
+  justify-content: left;
+}
+
+.header-input {
+  height: 25px;
+  border-radius: 5px;
+  border: 1px solid #000000;
+  text-align: center;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+.header-input::placeholder {
+    color: var(--placeholder-color);
+}
+
+.description {
+  text-align: left;
+  font-size: 12px;
+  margin-bottom: 3px;
+}
+
+#file-div {
+  width: 250px;
+  height: 180px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+  border-radius: 10px;
+  background-color: var(--textfield-background);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: -225px;
+}
+
+#file-upload {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#file-upload label {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+
+#file-upload input[type="file"] {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.upload-icon {
+  max-width: 50%;
+  max-height: 50%;
+  margin-bottom: -10px; /* Adjust margin to bring the text closer */
+  margin-left: 10px;
+  margin-top: -10px;
+}
+
+#upload-text {
+  color: var(--upload-text-color);
+  margin-top: 0; /* Remove any top margin to bring it closer to the image */
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px; /* Angepasst an die neue Höhe */
+  height: 20px;
+  margin-left: 20px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+  margin-left: 70px;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--slider-background-color);
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 20px; /* Angepasst an die neue Höhe */
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 16px; /* Angepasst an die neue Höhe */
+  width: 16px; /* Angepasst an die neue Höhe */
+  left: 2px; /* Angepasst an die neue Höhe */
+  bottom: 2px; /* Angepasst an die neue Höhe */
+  background-color: var(--create-page-background);
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: var(--cyan);
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px var(--cyan);
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(20px); /* Angepasst an die neue Höhe */
+  -ms-transform: translateX(20px); /* Angepasst an die neue Höhe */
+  transform: translateX(20px); /* Angepasst an die neue Höhe */
+}
+
+#main {
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  background-color: var(--create-page-background);
+  padding-top: 30px;;
+  gap: 20px;
+}
+
+.long-description {
+  border-radius: 10px;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+  padding: 10px;
+  display: grid;
+  grid-template-columns: 580px;
+  justify-content: left;
+  font-weight: bold;
+}
+
+.long-description::placeholder {
+    color: var(--placeholder-color);
+}
+
+#right-side-info {
+  border-radius: 10px;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+  padding: 10px;
+}
+
+#right-side-info::placeholder {
+    color: var(--placeholder-color);
+}
+
+.infos {
+  display: grid;
+  grid-template-columns: 180px;
+  margin-top: 20px;
+}
+
+.time-value-left {
+  width: 71.9px;
+  text-align: center;
+  border: 1px solid #000000; /* Rahmenstil */
+  border-radius: 5px; /* Abgerundete Ecken */
+  height: 25px;
+  margin-right: 5px;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+.time-value-left::placeholder {
+    color: var(--placeholder-color);
+}
+
+.time-value-right {
+  width: 71.9px;
+  text-align: center;
+  border: 1px solid #000000; /* Rahmenstil */
+  border-radius: 5px; /* Abgerundete Ecken */
+  height: 25px;
+  margin-left: 5px;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+.time-value-right::placeholder {
+    color: var(--placeholder-color);
+}
+
+.info-subheadline {
+  text-align: left;
+  font-size: 12px;
+}
+
+.input {
+  text-align: center;
+  border: 1px solid #000000; /* Rahmenstil */
+  border-radius: 5px; /* Abgerundete Ecken */
+  height: 25px;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+.input::placeholder {
+    color: var(--placeholder-color);
+}
+
+#long-description-input {
+  width: 558px;
+  height: 398px;
+  font-family: Arial, sans-serif;
+  font-size: 12px;
+  padding: 10px;
+  border: 1px solid #000000;
+  border-radius: 8px;
+  resize: none;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+#long-description-input::placeholder {
+    color: var(--placeholder-color);
+}
+
+#add-location-icon {
+  width: 25px;
+  height: 25px;
+  margin-top: 4px;
+}
+
+#add-location {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: var(--textfield-background);
+  color: var(--textfield-font-color);
+}
+
+#add-location::placeholder {
+    color: var(--placeholder-color);
+}
+
+#info-headline {
+  display: block; /* Als Block-Element anzeigen */
+  text-align: left; /* Text links ausrichten */
+  font-family: Arial, sans-serif; /* Schriftart festlegen */
+  font-size: 14px; /* Schriftgröße festlegen */
+  font-weight: bold;
+}
+
+#open-air {
+  display: block;
+  text-align: left;
+  margin-top: 20px;
+}
+
+#buttons {
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 15px;
+  color: var(--simple-font-color);
+}
+
+#break {
+  background-color: var(--red);
+  width: 88px;
+  height: 25px;
+  border-radius: 5px;
+  border: 1px solid #000000;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+#continue {
+  background-color: var(--green);
+  width: 88px;
+  height: 25px;
+  border-radius: 5px;
+  border: 1px solid #000000;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+#addcreator {
+  display: flex;
+  overflow-x: auto;
+  white-space: nowrap;
+  align-items: center;
+  padding: 20px 0;
+}
+
+.dish-item {
+  display: inline-block;
+  margin-right: 10px;
+  flex: 0 0 auto;
+}
+
+.add-dish-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+}
+
+#add-icon {
+  width: 24px;
+  height: 24px;
+}
+
+.dish-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+footer {
+  background-color: var(--create-page-background);
+}
+
+#dish-form {
+      display: grid;
+      grid-template-columns: auto auto;
+      justify-content: center;
+      align-items: center;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+      border-radius: 15px;
+      padding-left: 12px;
+      padding-top: 7px;
+      padding-bottom: 10px;
+      padding-right: 12px;
+      margin-left: 5px;
+  }
+
+  .upload-icon {
+      max-width: 50%;
+      max-height: 50%;
+      margin-bottom: -10px; /* Adjust margin to bring the text closer */
+      margin-left: 5px;
+      margin-top: -5px;
+  }
+  
+  #right {
+      display: grid;
+      grid-template-columns: auto;
+  }
+  
+  #text {
+    text-align: left;
+    font-size: 11px;
+    margin-top: 5px;
+  }
+  
+  #input {
+    width: 130px;
+    border-radius: 5px;
+    border: 1px solid #000;
+    margin-top: 5px;
+    font-size: 10px;
+    padding: 3px;
+    padding-left: 10px;
+    background-color: var(--textfield-background);
+    color: var(--textfield-font-color);
+}
+
+#input::placeholder {
+    color: var(--placeholder-color);
+}
+  
+  #ingredients {
+    text-align: left;
+    font-size: 10px;
+    margin-top: 5px;
+    border-top: 1px solid rgb(200, 200, 200);
+    padding-top: 5px;
+  }
 </style>
