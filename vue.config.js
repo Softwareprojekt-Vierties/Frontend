38d5954353762e115933abce770cd8db055b738f
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2024-07-11 11:35:39 +0200

    hotfix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+    pages: {
+        index: {
+            entry: "./src/main.js",
+            template: "./public/index.html",
+        }
+    },
   chainWebpack: config => {
 
     const svgRule = config.module.rule('svg');
